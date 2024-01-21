91e9b2e7f3a8345b780db1875ad2d43724bd9025
Jakob Gillich <jakob@gillich.me>
2024-01-21 15:40:35 +0100

    add license

--- ui/about_window.go
+++ ui/about_window.go
@@ -12,11 +12,10 @@ type AboutWindow struct {
 func NewAboutWindow(parent *gtk.Window) *AboutWindow {
 	w := AboutWindow{adw.NewAboutWindow()}
 	w.SetApplicationName(ApplicationName)
-	w.SetDeveloperName("Jakob Gillich")
 	w.SetVersion("0.1-dev")
 	w.SetTransientFor(parent)
 	w.SetWebsite("https://github.com/jgillich/kubegtk")
 	w.SetIssueURL("https://github.com/jgillich/kubegtk/issues")
-	w.SetLicenseType(gtk.LicenseGPL30)
+	w.SetLicenseType(gtk.LicenseMPL20)
 	return &w
 }
