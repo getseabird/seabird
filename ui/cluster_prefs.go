9e296ab25c1441b6d96cf5ac4a4a2a8717c03315
mgruszkiewicz <issei@issei.space>
2024-02-04 16:48:51 +0100

    ability to control environemtal variables from ui

--- ui/cluster_prefs.go
+++ ui/cluster_prefs.go
@@ -75,10 +75,12 @@ func NewClusterPrefPage(parent *gtk.Window, b *behavior.Behavior, active observe
 		p.executeProvider.command.SetText(string(prefs.ExecProvider.Command))
 		p.executeProvider.args.SetText(strings.Join(prefs.ExecProvider.Args, " "))
 
-		// TODO: add option to manipulate key:value environmental settings
-		for idx, val := range prefs.ExecProvider.Env {
-			fmt.Println("idx:", idx, "val name:", val.Name, "val value:", val.Value)
+		// Building string from key-value of environmentals
+		var envString strings.Builder
+		for _, val := range prefs.ExecProvider.Env {
+			envString.WriteString(fmt.Sprintf("%s=%s ", val.Name, val.Value))
 		}
+		p.executeProvider.env.SetText(envString.String())
 
 		p.executeProvider.provideClusterInfo.SetActive(prefs.ExecProvider.ProvideClusterInfo)
 
@@ -167,8 +169,15 @@ func (p *ClusterPrefPage) createContent() *adw.PreferencesPage {
 		p.executeProvider.apiversion.SetText(string(p.active.Value().ExecProvider.APIVersion))
 		p.executeProvider.command.SetText(string(p.active.Value().ExecProvider.Command))
 		p.executeProvider.args.SetText(strings.Join(p.active.Value().ExecProvider.Args, " "))
-		// p.executeProvider.env.SetText()
-		fmt.Println(p.active.Value().ExecProvider.Env)
+
+		// Building string from key-value of environmentals
+		var envString strings.Builder
+		for _, val := range p.active.Value().ExecProvider.Env {
+			envString.WriteString(fmt.Sprintf("%s=%s ", val.Name, val.Value))
+		}
+		p.executeProvider.env.SetText(envString.String())
+
+		// Get and set selected interactiveMode
 		for idx, val := range AuthInteractiveModes {
 			if val == string(p.active.Value().ExecProvider.InteractiveMode) {
 				p.executeProvider.interactiveMode.SetSelected(uint(idx))
@@ -302,6 +311,25 @@ func (p *ClusterPrefPage) createSaveButton() *gtk.Button {
 		cluster.ExecProvider.APIVersion = p.executeProvider.apiversion.Text()
 		cluster.ExecProvider.Command = p.executeProvider.command.Text()
 		cluster.ExecProvider.Args = strings.Fields(p.executeProvider.args.Text())
+		// Not all ExecuteProvider require environemntal variables
+		if p.executeProvider.env != nil {
+			var execEnvVar []api.ExecEnvVar
+			if p.executeProvider.env.Text() != "" {
+				pairs := strings.Split(p.executeProvider.env.Text(), " ")
+				for _, pair := range pairs {
+					// Split each pair by '='
+					keyValue := strings.Split(pair, "=")
+					if len(keyValue) != 2 {
+						ShowErrorDialog(p.parent, "Invalid key-value environemtal pair", errors.New(fmt.Sprintf("%s is invalid", keyValue)))
+						return
+					}
+					// Append ExecEnvVar key value pair
+					execEnvVar = append(execEnvVar, api.ExecEnvVar{Name: keyValue[0], Value: keyValue[1]})
+				}
+			}
+			cluster.ExecProvider.Env = execEnvVar
+		}
+
 		switch p.executeProvider.interactiveMode.Selected() {
 		case 0:
 			cluster.ExecProvider.InteractiveMode = api.NeverExecInteractiveMode
