25af62b9f4e9e93fe06513d5ab8c21087a8247ff
Jakob Gillich <jakob@gillich.me>
2024-04-03 23:10:10 +0200

    buld(darwin): add common bin paths

--- build/darwin/seabird.sh
+++ build/darwin/seabird.sh
@@ -7,4 +7,8 @@ export GSETTINGS_SCHEMA_DIR="$LAUNCH_DIR/share/glib-2.0/schemas"
 export GDK_PIXBUF_MODULEDIR="$LAUNCH_DIR/lib/gdk-pixbuf-2.0"
 export GDK_PIXBUF_MODULE_FILE="$LAUNCH_DIR/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
 export XDG_DATA_DIRS="$LAUNCH_DIR/share"
+
+# add common bin paths for exec plugin discoverability
+export PATH="$PATH:/opt/homebrew/bin:/opt/local/bin"
+
 "$LAUNCH_DIR/seabird"
